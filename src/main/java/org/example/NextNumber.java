6018da615478d731785bd83c047a03f40ac2b2bb
paran <paran2586@naver.com>
2022-11-04 11:15:54 +0900

    [2022-11-04] 프로그래머스 문제풀이 (테스트 케이스에서 오류가 나서 다시 수정)

--- src/main/java/org/example/NextNumber.java
+++ src/main/java/org/example/NextNumber.java
@@ -4,28 +4,45 @@ public class NextNumber {
     public static int solution(int[] common) {
         int answer = 0;
         int Operator = 0;
+        int a = common[common.length-1]-common[common.length-2];
+        System.out.println("a--> " + a);
+        int b = common[common.length-2]-common[common.length-3];
+        System.out.println("b--> " + b);
 
-        if((common[1] - common[0]) == common[2]-common[1]){
+        if(a == b){
             Operator = common[1]-common[0];
+            System.out.println(Operator);
             answer = max(common) + Operator;
-        } else if(common[1] / common[0] == common[2] / common[1]){
-            Operator = common[1] / common[0];
+        }
+        else {
+            Operator = common[common.length-1] / common[common.length-2];
             answer = max(common) * Operator;
         }
+
         return answer;
     }
 
     public static int max(int[] common){
-        int max = 0;
+        int max = common[0];
+        int min = common[0];
+
         for(int i = 0; i < common.length ; i++){
             if(max < common[i])
                 max = common[i];
+            System.out.println("max - > " + max);
+            if(min > common[i])
+                min = common[i];
+            System.out.println("min -> " + min);
+            if(common[common.length-1] < common[common.length-2]){
+                max = min;
+            }
         }
+
         return max;
     }
 
     public static void main(String [] args){
-        int a = solution(new int[]{2,4,8});
+        int a = solution(new int[]{2,4,8,16});
         System.out.println(a);
     }
 }
