80f95aaa25ea6c72bd08649ff20fc1a1f0cea5b8
paran <paran2586@naver.com>
2022-11-05 22:41:29 +0900

    [2022-11-05] 프로그래머스 문제 풀이 및 리팩토링

--- src/main/java/org/example/babySaying.java
+++ src/main/java/org/example/babySaying.java
@@ -2,31 +2,27 @@ package org.example;
 
 public class babySaying {
     public static int solution(String[] babbling) {
-        String [] baby = {"aya","ye","woo","ma"};
-
-        int cnt = 0;
         int answer = 0;
-        String changeStr = "";
+        for(String i : babbling){
+            String defaultBabbling = i;
 
-        while(cnt < babbling.length){
-            String babblingStr = babbling[cnt];
-            for(int i = 0; i < baby.length ; i++){
-                String babyStr = baby[i];
-                if(babblingStr.contains(babyStr)) {
-                    babblingStr = babblingStr.replace(babyStr, ".");
-                }
-            }
-            babblingStr = babblingStr.replace(".","");
-            if(babblingStr.length()==0){
+            if(defaultBabbling.contains("aya")) i = i.replaceFirst("aya","");
+            System.out.println("aya - > "  + i);
+            if(defaultBabbling.contains("ye")) i = i.replaceFirst("ye","");
+            System.out.println("ye - > "  + i);
+            if(defaultBabbling.contains("woo")) i = i.replaceFirst("woo","");
+            System.out.println("woo - > "  + i);
+            if(defaultBabbling.contains("ma")) i = i.replaceFirst("ma","");
+            System.out.println("ma - > "  + i);
+            if(i.equals("")){
                 answer++;
             }
-            cnt++;
         }
 
         return answer;
     }
     public static void main(String [] args){
-        int a = solution(new String[]{"ayaye","uuuma", "ye", "yemawoo", "ayaa"});
+        int a = solution(new String[]{"aya", "yee", "u", "maa", "wyeoo"});
         System.out.println("int a ==> " + a);
     }
 
